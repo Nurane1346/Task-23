3ec103a22b79972275949d6eebec567a14775e9a
Nurane1346 <nuraneh01@gmail.com>
2024-01-12 16:41:52 +0400

    created file

---
+++ main.js
@@ -0,0 +1,89 @@
+// Task01
+// console.log("My name is Nurane");
+
+// Task 02
+// const name="Nurane";
+// console.log(name);
+
+// Task 03
+// person
+// $add
+
+// Task 04
+
+
+
+// Task 05
+// let kmhSpeed = 75;
+// let msSpeed = 75*1000/3600;
+// console.log(msSpeed);
+
+// Task 06
+// let year = 1996;
+// let age = (2024-1996)*360;
+// console.log(age);
+
+// Task 07
+//  let num = 10**5;
+//  let end = (10**5)/3;
+//  console.log(end)
+
+//  Task 8
+// let firstName = 'Alan';
+// let lastName = 'Turing';
+// console.log(`Sizin adınız ${firstName} ${lastName}.Tanışlığımıza çox şadam!`);
+
+// Task 9
+// let firstName = "Nermin";
+// let lastName = "Alxasova";
+// console.log(`Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName}.`)
+
+// Task 10
+// let fullName = "Hesenova Nurane";
+// let add = fullName.length;
+// console.log(add)
+
+// Task 11
+// let name = 'Nurane';
+// let surname = "Hesenova";
+// let fullName = name.charAt(0)+surname.charAt(0);
+// console.log(fullName)
+ 
+// Task 12
+
+
+// Task 13
+// alert("Salam, Nurane");
+
+// Task 14
+
+// Task 15
+
+// Task 16
+// "" + 1 + 0 ="10"
+
+// "" - 1 + 0 = Nan
+
+// "true" - false =Nan
+
+// 6 / "3" =2
+
+// 4 + 5 + "px" =9px
+
+// "$" + 4 +5 =  $45
+
+// "4" - 2 =2
+
+// "4px" - 2 + "px" =Nanpx
+
+// 7 / 0 =0
+
+// "-9" + 5 =-95
+
+// "-9" - 5 =-14
+
+// null + 1 =1
+
+// undefined + 1 =Nan1
+
+
